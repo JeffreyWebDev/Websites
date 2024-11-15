c320ed36edc691f6bfc1b7d036625afcc4baffee
Jeffrey Faray <158853324+JeffreyWebDev@users.noreply.github.com>
2024-11-15 13:14:27 +0300

    Initial commit

---
+++ 06.Shopping Cart/products.js
@@ -0,0 +1,59 @@
+const products = [
+    {
+        "id": 1,
+        "name": "LD01 LOUNGE CHAIR",
+        "price": 200,
+        "description": "Expertly rendered by Carl Hansen and Son, the lounge chair-first ",
+        "image": "images/chair1.png"
+    },  
+    {
+        "id": 2,
+        "name": "LD02 LOUNGE CHAIR",
+        "price": 300,
+        "description": "Expertly rendered by Carl Hansen and Son, the lounge chair-first ",
+        "image": "images/chair2.png"
+    },  
+    {
+        "id": 3,
+        "name": "LD03 LOUNGE CHAIR",
+        "price": 350,
+        "description": "Expertly rendered by Carl Hansen and Son, the lounge chair-first ",
+        "image": "images/chair3.png"
+    },  
+    {
+        "id": 4,
+        "name": "LD04 LOUNGE CHAIR",
+        "price": 400,
+        "description": "Expertly rendered by Carl Hansen and Son, the lounge chair-first ",
+        "image": "images/chair2.png"
+    },  
+    {
+        "id": 5,
+        "name": "LD05 LOUNGE CHAIR",
+        "price": 250,
+        "description": "Expertly rendered by Carl Hansen and Son, the lounge chair-first ",
+        "image": "images/chair3.png"
+    },  
+    {
+        "id": 6,
+        "name": "LD06 LOUNGE CHAIR",
+        "price": 190,
+        "description": "Expertly rendered by Carl Hansen and Son, the lounge chair-first ",
+        "image": "images/chair1.png"
+    },  
+    {
+        "id": 7,
+        "name": "LD07 LOUNGE CHAIR",
+        "price": 230,
+        "description": "Expertly rendered by Carl Hansen and Son, the lounge chair-first ",
+        "image": "images/chair2.png"
+    },  
+    {
+        "id": 8,
+        "name": "LD08 LOUNGE CHAIR",
+        "price": 340,
+        "description": "Expertly rendered by Carl Hansen and Son, the lounge chair-first ",
+        "image": "images/chair1.png"
+    },  
+]
+export default products;
